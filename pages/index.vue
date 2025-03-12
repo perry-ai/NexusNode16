67058447070819dcd90759e1cb0b88e478ad323e
perry-ai <perry-ai@outlook.com>
2025-03-13 00:33:43 +0800

    refactor:project name

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">nuxt0302</h1>
-      <h2 class="subtitle">My Nuxt.js project</h2>
+      <h1 class="title">MyTools</h1>
+      <h2 class="subtitle"></h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green"> Documentation </a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey"> GitHub </a>
