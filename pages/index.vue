2e098fa48e1ee69c6b25cb63c850a4f1b9fa2483
perry-ai <perry-ai@outlook.com>
2025-04-03 01:37:07 +0800

    feat: v0.1.0 index page

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,7 @@
     <div>
       <logo />
       <h1 class="title">NexusNode16</h1>
-      <h2 class="subtitle"></h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"> Documentation </a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey"> GitHub </a>
-      </div>
+      <h2 class="subtitle">联结点拾陆</h2>
     </div>
   </div>
 </template>
